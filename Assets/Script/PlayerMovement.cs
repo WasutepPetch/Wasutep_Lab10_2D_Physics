ddde7d14e63b055fff0e65d1a4fd390529c2e603
WasutepPetch <wasutep.khun@bumail.net>
2026-04-24 23:34:26 +0700

    Jump Code

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,16 +1,25 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb2d;
     float move;
-    [SerializeField] float; speed;
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        
+        move Input = GetAxis("Horizontal");
+        rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);
     }
-}
+    If { Input.GetButtonDown("Jump")){
+        rb2d.AddForce(new Vector2 (rb2d.linearVelocity.x, jumpForce ));
+        Debug.Log("Jump!");
+    }
+
+
+
